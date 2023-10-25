5ff8054bb90be6573bea94e5f76b9c0a34786722
NikitaPas <nikita223372@gmail.com>
2023-10-25 21:26:54 +0300

    feat: added internal class Attraction

--- JavaCourse/src/main/java/Lesson_4/Park.java
+++ JavaCourse/src/main/java/Lesson_4/Park.java
@@ -1,12 +1,15 @@
 package Lesson_4;
 
 public class Park {
-    String name;
-    int workTime;
-    int cost;
-    public Park(String name, int workTime, int cost) {
-        this.name = name;
-        this.workTime = workTime;
-        this.cost = cost;
+    public class Attraction {
+        String name;
+        int workTime;
+        int cost;
+
+        public Attraction(String name, int workTime, int cost) {
+            this.name = name;
+            this.workTime = workTime;
+            this.cost = cost;
+        }
     }
 }
